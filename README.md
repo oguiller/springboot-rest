7419f3d4cfd7a5e415f75fe6ca8f15f32ad6f01b
Guillermo Rodriguez <xile@klm134953.cs.ad.klmcorp.net>
2018-12-29 00:02:43 +0100

    Initiat setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# A testing sandbox for SpringBoot
---
+++ src/test/java/com/oguiller/springboot/HelloWorldControllerTest.java
@@ -0,0 +1,28 @@
+package com.oguiller.springboot;
+
+import com.oguiller.springboot.controller.HelloWorldController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+  @Test
+  public void testHelloWorldController() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/"))
+      .andExpect(MockMvcResultMatchers.status().isOk())
+      .andExpect(content().string("Hello World, from Spring Boot 2!"))
+      .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN));
+  }
+}
